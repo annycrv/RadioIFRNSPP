5a7e83d8716677b747abcbe981949d1f219e76fb
Anny Carolline <annacaroline.8002@gmail.com>
2025-10-15 17:54:12 -0300

    Adicionando páginas HTML

--- radio/urls.py
+++ radio/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pedidos/', views.pedidos, name='pedidos'),
+    path('programacao/', views.programacao, name='programacao'),
+    path('programas/', views.programas, name='programas'),
+    path('podcasts/', views.podcasts, name='podcasts'),
+    path('sobre/', views.sobre, name='sobre'),
 ]
--- radio/views.py
+++ radio/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "radio/index.html")
+
+def pedidos(request):
+    return render(request, "radio/pedidos.html")
+
+def programacao(request):
+    return render(request, "radio/programacao.html")
+
+def programas(request):
+    return render(request, "radio/programas.html")
+
+def podcasts(request):
+    return render(request, "radio/podcasts.html")
+
+def sobre(request):
+    return render(request, "radio/sobre.html")
