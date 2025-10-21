82da6c20b05c5acf3787aa797635faa42531f288
annycrv <annacaroline.8002@gmail.com>
2025-10-21 08:57:42 -0300

    Adicionando Models

--- radio/admin.py
+++ radio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Home, Programacao,Programa, Podcast, Sobre
+
+admin.site.register(Home)
+admin.site.register(Programacao)
+admin.site.register(Programa)
+admin.site.register(Podcast)
+admin.site.register(Sobre)
+
--- radio/models.py
+++ radio/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class Home(models.Model):
+    titulo_home = models.CharField(max_length=100)
+    subtitulo_home = models.CharField(max_length=200)
+    bg_home = models.ImageField(upload_to="radio/banner_home/", blank=True)
+    quemsomos_home = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Home"
+
+    def __str__ (self):
+        return self.titulo
+
+class Programacao(models.Model):
+    bg_programação = models.ImageField(upload_to="radio/img_programacao/", blank=True)
+    titulo_programacao = models.CharField(max_length=100)
+    horario_programacao = models.TimeField()
+
+    class Meta:
+        verbose_name_plural = "Programações"
+
+    def __str__ (self):
+        return self.titulo
+
+class Programa(models.Model):
+    banner_programa = models.ImageField(upload_to="radio/img_programa/", blank=True)
+    nome_programa = models.CharField(max_length=100)
+    descricao_programa = models.CharField(max_length=100)
+    artista_programa = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Programas"
+
+    def __str__ (self):
+        return self.titulo
+
+class Podcast(models.Model):
+    banner_podcast = models.ImageField(upload_to="radio/img_podcast/", blank=True)
+    nome_podcast = models.CharField(max_length=100)
+    descricao_podcast = models.CharField(max_length=100)
+    host_podcast = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Podcasts"
+
+    def __str__ (self):
+        return self.titulo
+
+class Sobre(models.Model):
+    titulo_sobre = models.CharField(max_length=100)
+    subtitulo_sobre = models.CharField(max_length=150)
+    conteudo_sobre = models.CharField(max_length=150)
+    banner_sobre = models.ImageField(upload_to="banner_sobre/")
+
+    class Meta:
+        verbose_name_plural = "Sobre"
+
+    def __str__ (self):
+        return self.titulo
