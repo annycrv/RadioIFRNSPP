0992a680ff39031e2dde74d6a677d8aa5e7c9ad3
Williany Vitoria <willianyvitoria2007@gmail.com>
2025-11-09 01:35:15 -0300

    adicionando partials (WIP)

--- dashboard/views.py
+++ dashboard/views.py
@@ -18,12 +18,20 @@ def index(request):
 def quadros(request):
     context = {
         "quadros": Quadro.objects.all(),
+        "titulo_pagina": "Quadros",
+        "subtitulo_pagina": "Gerencie os quadros da rádio",
+        "url_novo": "dashboard:quadro_novo",
+        "partial_tabela": "dashboard/partials/_tabela_quadros.html",
+        "texto_botao_novo": "Adicionar Quadro", 
     }
-    return render(request, "dashboard/quadros.html", context)
+    return render(request, "dashboard/listar.html", context)
 
 
 def quadro_novo(request):
-    context = {}
+    context = { 
+        "titulo_pagina": "Adicionar quadro",
+        "url_cancelar": "dashboard:quadros",
+    }
     if request.method == "POST":
         form = QuadroModelForm(request.POST,request.FILES)
         if form.is_valid():
@@ -33,7 +41,7 @@ def quadro_novo(request):
             context["form"] = form
     else:
         context["form"] = QuadroModelForm()
-    return render(request, "dashboard/quadro_novo.html", context)
+    return render(request, "dashboard/novo.html", context)
 
 # def quadro_detalhar(request, id_post):
 #     return render(request,"dashboard/quadro_detalhar.html",{"post": get_object_or_404(Quadro, id=id_post)})
@@ -41,6 +49,8 @@ def quadro_novo(request):
 def quadro_editar(request, id_quadro):
     context = {
         "quadro": get_object_or_404(Quadro, id=id_quadro),
+        "titulo_pagina": "Editar quadro",
+        "url_cancelar": "dashboard:quadros",
     }
     if request.method == "POST":
         form = QuadroModelForm(request.POST, instance=context["quadro"])
@@ -51,7 +61,7 @@ def quadro_editar(request, id_quadro):
             context["form"] = form
     else:
         context["form"] = QuadroModelForm(instance=context["quadro"])
-    return render(request, "dashboard/quadro_editar.html", context)
+    return render(request, "dashboard/editar.html", context)
 
 
 def quadro_remover(request, id_quadro):
@@ -69,11 +79,21 @@ def quadro_remover(request, id_quadro):
 def programacao(request):
     context = {
         "programacao": Programacao.objects.all(),
+        "titulo_pagina": "Programação",
+        "subtitulo_pagina": "Gerencie a programação da rádio",
+        "url_novo": "dashboard:programacao_novo",
+        "partial_tabela": "dashboard/partials/_tabela_programacoes.html",
+        "texto_botao_novo": "Adicionar Programação", 
+
+
     }
-    return render(request, "dashboard/programacao.html", context)
+    return render(request, "dashboard/listar.html", context)
 
 def programacao_novo(request):
-    context = {}
+    context = {
+        "titulo_pagina": "Adicionar Programação",
+        "url_cancelar": "dashboard:programacao",
+    }
     if request.method == "POST":
         form = ProgramacaoModelForm(request.POST,request.FILES)
         if form.is_valid():
@@ -83,11 +103,13 @@ def programacao_novo(request):
             context["form"] = form
     else:
         context["form"] = ProgramacaoModelForm()
-    return render(request, "dashboard/programacao_novo.html", context)
+    return render(request, "dashboard/novo.html", context)
 
 def programacao_editar(request, id_item):
     context = {
         "programacao": get_object_or_404(Programacao, id=id_item),
+        "titulo_pagina": "Editar Programação",
+        "url_cancelar": "dashboard:programacao",
     }
     if request.method == "POST":
         form = ProgramacaoModelForm(request.POST, instance=context["programacao"])
@@ -98,7 +120,7 @@ def programacao_editar(request, id_item):
             context["form"] = form
     else:
         context["form"] = ProgramacaoModelForm(instance=context["programacao"])
-    return render(request, "dashboard/programacao_editar.html", context)
+    return render(request, "dashboard/editar.html", context)
 
 def programacao_remover(request, id_item):
     context = {
